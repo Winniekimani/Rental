780f8fc36de5ca7d2ae8a715fbc0ab57a7972e63
Winniekimani <kimaniwinniew@gmail.com>
2023-11-29 23:59:30 +0300

    method injection

--- src/main/java/com/winnie/app/bean/HouseBean.java
+++ src/main/java/com/winnie/app/bean/HouseBean.java
@@ -4,8 +4,6 @@ package com.winnie.app.bean;
 import com.winnie.app.model.entity.House;
 import com.winnie.app.model.entity.HouseType;
 import com.winnie.database.MysqlDatabase;
-import com.winnie.utility.ReferenceNumberGenerator;
-import com.winnie.utility.RentCalculator;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -13,7 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -28,58 +25,18 @@ public class HouseBean extends GenericBean<House> implements HouseBeanI, Seriali
   /*  HouseBean houseBean = new HouseBean();
     List<House> houseList = houseBean.list();*/
 
-    @Inject
-    private RentCalculator rentCalculator;
-
-    @Inject
-    private ReferenceNumberGenerator referenceNumberGenerator;
-
-
     public List<House> list() {
         // You can directly call the list method from GenericBean
+        return super.list(House.class);
+    }
 
-        List<House> houseList = super.list(House.class);
-        // Calculate rent for each house using Injected RentCalculator
-        for (House house : houseList) {
-            BigDecimal calculatedRent = rentCalculator.calculateRent(house);
-            house.setCalculatedRent(calculatedRent);
-        }
-      /*  return super.list(House.class);*/
-        return houseList;
+
+    @Inject
+    public void tryMethodInjection(){
+        System.out.println("method injection trial............");
     }
 
 
 
-   /* public List<House> list(Class<?> entity) {
-        List<House> houseList = new ArrayList<>();
-        String sql = "SELECT name, type, location, price FROM house";
-
-        try {
-            Connection conn = MysqlDatabase.getInstance().getConnection();
-
-
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-                while (resultSet.next()) {
-                    House house = new House();
-                    //house.setHouseId(resultSet.getString("id"));
-                    house.setHouseName(resultSet.getString("name"));
-                    house.setHouseType(HouseType.valueOf(resultSet.getString("type")));
-                    house.setHouseLocation(resultSet.getString("location"));
-                    house.setHousePrice(resultSet.getBigDecimal("price"));
-
-                    houseList.add(house);
-
-                }
-                return houseList;
-
-
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 }
 
